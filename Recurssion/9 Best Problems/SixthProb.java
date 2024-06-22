01f5bb040688c36a1b34fec370b1d58c0e39af5e
Tushar k <tushar.kale@mitcorer.edu.in>
2024-06-22 22:38:50 +0530

    Sixthproblem in recurssion using java

---
+++ Recurssion/9 Best Problems/SixthProb.java
@@ -0,0 +1,25 @@
+//Remove Duplicates in a string
+public class SixthProb {
+    public static boolean[]map = new boolean [26];
+
+    public static void removeduplicates(String str,int idx,String newsString){
+        if (idx==str.length()) {
+            System.out.println(newsString);
+            return;
+        }
+
+        char Currchar = str.charAt(idx);
+        if (map[Currchar-'a']==true) {
+            removeduplicates(str, idx+1, newsString);
+        }else{
+            newsString+=Currchar;
+            map[Currchar-'a']=true;
+            removeduplicates(str, idx+1, newsString);
+
+        }
+    }  
+    public static void main(String[] args) {
+        String str = "abbccda";
+        removeduplicates(str, 0, "");
+    }   
+}
