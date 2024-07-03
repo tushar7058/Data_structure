9b3df1d71c4e33641b4a04864b933994b7abd3a8
Tushar k <tushar.kale@mitcorer.edu.in>
2024-07-03 16:27:34 +0530

    seventh prob in recursion using java

---
+++ Recurssion/9 Best Problems/seventhProb.java
@@ -0,0 +1,18 @@
+// print all the subsequence of a string
+public class seventhProb {
+    public static void subsequences(String str, int index,String NewStirng){
+        if (index == str.length()) {
+            System.out.println(NewStirng);
+            return;
+        }
+        char currChar = str.charAt(index);
+         // choices to be 
+         subsequences(str, index+1, NewStirng+currChar);
+         // not to be
+         subsequences(str, index+1, NewStirng);
+    }
+    public static void main(String[] args) {
+        String str = "abc";
+        subsequences(str, 0, "");
+    }
+}
